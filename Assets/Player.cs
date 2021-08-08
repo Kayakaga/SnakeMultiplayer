dfc862e445fb092afffbc0360b55becd31f4a750
Kayakaga <baijidevelop@gmail.com>
2021-08-08 12:05:06 +0300

    Snake Movement + Food Spawning

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,15 +1,44 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Player : MonoBehaviour
 {
+    List<Transform> tail = new List<Transform>();
+    Vector2 dir = Vector2.right;
     void Start()
     {
-        
+        InvokeRepeating("Move", 0.3f, 0.3f);
     }
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            dir = Vector2.right;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            dir = Vector2.down;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            dir = Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            dir = Vector2.up;
+        }
+    }
+    void Move()
+    {
+        Vector2 v = transform.position;
+        transform.Translate(dir);
+        if(tail.Count > 0)
+        {
+            tail.Last().position = v;
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count-1);
+        }
     }
 }
