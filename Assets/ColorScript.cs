968c8f38c663c5eb71e50fb57475799285da1591
Kayakaga <baijidevelop@gmail.com>
2021-08-10 14:59:49 +0300

    Test scene: working transform code for player that all the clients can control (No Authority Required - NAR)

--- Assets/ColorScript.cs
+++ Assets/ColorScript.cs
@@ -5,9 +5,11 @@ using Mirror;
 
 public class ColorScript : NetworkBehaviour
 {
+    Vector2 dir;
     SpriteRenderer sr;
     [SyncVar]
     Color newColor;
+
     [ClientCallback]
     void Start()
     {
@@ -15,15 +17,36 @@ public class ColorScript : NetworkBehaviour
         CmdCallColor();
         InvokeRepeating("CmdCallColor", 1f, 1f);
     }
-    [Command]
+    [ClientCallback]
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            dir = Vector2.right;
+            transform.Translate(dir);
+            CmdMove(transform.position);
+        }
+    }
+    [Command(requiresAuthority = false)]
+    void CmdMove(Vector2 pos)
+    {
+        RpcMove(pos);
+    }
+    [ClientRpc]
+    void RpcMove(Vector2 pos)
+    {
+        transform.position = pos;
+    }
+
+    [Command(requiresAuthority = false)]
     void CmdCallColor()
     {
+        newColor = new Color(Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), 1);
         RpcChangeColor();
     }
     [ClientRpc]
     void RpcChangeColor()
     {
-        newColor = new Color(Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), 1);
         sr.color = newColor;
     }
 }
